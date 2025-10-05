040fb76e095c4829eea729c516854ee1ee1dcf63
murliworld <murlivishwakarma4@gmail.com>
2025-10-05 16:40:06 +0530

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,7 +2,7 @@
 import { customAxios } from "./helper";
 export async function sendEmail(emailData){
 
-  const result=(await customAxios.post(`/send-html`, emailData) ).data;
+  const result=(await customAxios.post(`/api/email/v1/send-html`, emailData) ).data;
 
   return result;
 }
